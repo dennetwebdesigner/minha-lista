e39d960945006aa5697d97b858ff18271f93206b
Joao Marcon <jvmldev@gmail.com>
2023-03-03 14:10:54 -0300

    FA: Uploading database

--- api/src/Database/Database.php
+++ api/src/Database/Database.php
@@ -1,6 +1,7 @@
 <?php
 
-namespace MyList\
+namespace Mylist\Mylist\Database;
+
 class Database
 {
     private string $_host = 'localhost';
@@ -8,13 +9,12 @@ class Database
     private string $_username = 'joatan';
     private string $_password = '123';
     private object $_connect;
-    //joatan 123
+
     public function __construct()
     {
         try {
             $this->_connect = new PDO("mysql:host={$this->_host};dbname={$this->_database}", $this->_username, $this->_password);
             $this->_connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            echo 'connect in database';
         } catch (PDOException $e) {
             throw new Exception('Error in connect database: ' . $e->getMessage());
         }
